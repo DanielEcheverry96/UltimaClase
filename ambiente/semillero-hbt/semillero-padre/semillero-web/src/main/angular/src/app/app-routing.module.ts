f488e6fa198a31a5e0e1efaa5e6f5f7c79b9433b
ccastano <ccastano@heinsohn.com.co>
2019-04-11 18:23:22 -0500

    Comunicacion entre componentes.

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app-routing.module.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreacionPersonasComponent } from './nucleo/capa/principal/creacion-personas/creacion-personas.component'
+import { CreacionPersonasComponent } from './nucleo/capa/principal/creacion-personas/creacion-personas.component';
+import { ApoyoComponent } from './nucleo/capa/principal/apoyo/apoyo.component';
+import { GestionVehiculosComponent } from './nucleo/capa/principal/gestion_vehiculos/gestion_vehiculos.component';
+
 const routes: Routes = [
-	{
-	    path: 'personas-crear',
+	{   path: 'personas-crear',
 	    component: CreacionPersonasComponent
+  	},
+  	{   path: 'componente-apoyo',
+	    component: ApoyoComponent
+  	},
+  	{   path: 'vehiculos-gestion',
+	    component: GestionVehiculosComponent
   	},
 ];
 
